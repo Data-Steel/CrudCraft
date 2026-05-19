f7b7cd7651acba8c5452c3f5ce4f061cba76bc34
FlorisBeentjes <floris@fbeentjes.nl>
2026-05-19 14:16:28 +0200

    fix: add missing license headers

--- crudcraft-starter-projection/src/main/java/nl/datasteel/crudcraft/starter/projection/CrudCraftProjectionStarter.java
+++ crudcraft-starter-projection/src/main/java/nl/datasteel/crudcraft/starter/projection/CrudCraftProjectionStarter.java
@@ -2,7 +2,7 @@
  * Copyright (c) 2026 CrudCraft contributors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * You may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package nl.datasteel.crudcraft.starter.projection;
 
 /**
